a35670cb02c68d160af563cc46cbb5caf8542213
Felix Lange <fl@travelping.com>
2011-12-02 17:38:33 +0100

    log RADIUS replies

--- src/eradius_server.erl
+++ src/eradius_server.erl
@@ -232,7 +232,7 @@ handle_request({HandlerMod, HandlerArg}, NasProp, EncRequest) ->
             Sender = {NasProp#nas_prop.nas_ip, NasProp#nas_prop.nas_port, Request#radius_request.reqid},
             {ok, RadiusLog} = eradius_log:open(),
             eradius_log:write_request(RadiusLog, Sender, Request),
-            apply_handler_mod(HandlerMod, HandlerArg, Request, NasProp);
+            apply_handler_mod(HandlerMod, HandlerArg, Request, NasProp, RadiusLog);
         bad_pdu ->
             {discard, bad_pdu}
     end.
@@ -257,11 +257,13 @@ nas_prop_tuple_to_record({nas_prop_v1, ServerIP, ServerPort, NasIP, NasPort, Sec
               nas_ip = NasIP, nas_port = NasPort,
               secret = Secret, trace = Trace, handler_nodes = Nodes}.
 
--spec apply_handler_mod(module(), term(), #radius_request{}, #nas_prop{}) -> {discard, term()} | {exit, term()} | {reply, binary()}.
-apply_handler_mod(HandlerMod, HandlerArg, Request, NasProp) ->
+-spec apply_handler_mod(module(), term(), #radius_request{}, #nas_prop{}, eradius_log:log()) -> {discard, term()} | {exit, term()} | {reply, binary()}.
+apply_handler_mod(HandlerMod, HandlerArg, Request, NasProp, RadiusLog) ->
     try HandlerMod:radius_request(Request, NasProp, HandlerArg) of
-        {reply, #radius_request{cmd = ReplyCmd, attrs = ReplyAttrs}} ->
+        {reply, Reply = #radius_request{cmd = ReplyCmd, attrs = ReplyAttrs}} ->
+            Sender = {NasProp#nas_prop.nas_ip, NasProp#nas_prop.nas_port, Request#radius_request.reqid},
             EncReply = eradius_lib:encode_reply_request(Request#radius_request{cmd = ReplyCmd, attrs = ReplyAttrs}),
+            eradius_log:write_request(RadiusLog, Sender, Reply),
             {reply, EncReply};
         noreply ->
             {discard, handler_returned_noreply};
