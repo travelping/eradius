73c455dec4e4d8d635e3d6da96cb3328247ebd04
Dima Aleksandrov <elixmoon@gmail.com>
2014-07-16 16:29:03 +0200

    [eradius] Initial implementation of resend
    Test Plan: read
    
    Reviewers: aschultz, orixmann
    
    Reviewed By: orixmann
    
    Subscribers: aschultz, druss, mkraemer
    
    Maniphest Tasks: T567
    
    Differential Revision: https://review.tpip.net/D1561

--- src/eradius_server.erl
+++ src/eradius_server.erl
@@ -182,7 +182,8 @@ do_radius(ServerPid, ReqKey, Handler = {HandlerMod, _}, NasProp, {udp, Socket, F
             gen_udp:send(Socket, FromIP, FromPort, EncReply),
             ServerPid ! {replied, ReqKey, self()},
             eradius_counter:inc_counter(replies, NasProp),
-            wait_resend(ServerPid, ReqKey, FromIP, FromPort, EncReply, ?RESEND_RETRIES);
+            {ok, ResendTimeout} = application:get_env(eradius, resend_timeout),
+            wait_resend_init(ServerPid, ReqKey, FromIP, FromPort, EncReply, ResendTimeout, ?RESEND_RETRIES);
         {discard, Reason} ->
             dbg(NasProp, "discarding request ~p: ~1000.p~n", [ReqKey, Reason]),
             discard_inc_counter(Reason, NasProp),
@@ -199,15 +200,18 @@ discard_inc_counter(bad_pdu, NasProp) ->
 discard_inc_counter(_Reason, NasProp) ->
     eradius_counter:inc_counter(packetsDropped, NasProp).
 
+wait_resend_init(ServerPid, ReqKey, FromIP, FromPort, EncReply, ResendTimeout, Retries) ->
+    erlang:send_after(ResendTimeout, self(), timeout),
+    wait_resend(ServerPid, ReqKey, FromIP, FromPort, EncReply, Retries).
+
 wait_resend(ServerPid, ReqKey, _FromIP, _FromPort, _EncReply, 0) ->
     ServerPid ! {discarded, ReqKey};
 wait_resend(ServerPid, ReqKey, FromIP, FromPort, EncReply, Retries) ->
     receive
         {ServerPid, resend, Socket} ->
             gen_udp:send(Socket, FromIP, FromPort, EncReply),
-            wait_resend(ServerPid, ReqKey, FromIP, FromPort, EncReply, Retries - 1)
-    after
-        ?RESEND_TIMEOUT ->
+            wait_resend(ServerPid, ReqKey, FromIP, FromPort, EncReply, Retries - 1);
+        timeout ->
             ServerPid ! {discarded, ReqKey}
     end.
 
